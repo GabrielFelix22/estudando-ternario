6cbb8b972c37f6c79c83e6578cdfc6ce0e5caa99
Gabriel Felix <gabriel.felix@smartsky.tech>
2025-07-01 17:20:44 -0300

    commit inclusao contagem de funcionários, inclusao de menssagem para lista de funcionários quando vazia.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import './App.css';
+import {
+  Plus,
+  ToggleLeft,
+  ToggleRight,
+  Trash2,
+  TvMinimal,
+  User,
+} from 'lucide-react';
 import { useEffect, useState } from 'react';
-import { TvMinimal, User, ToggleLeft, ToggleRight, Plus, Trash2 } from 'lucide-react';
 
 function App() {
   const [seguindo, setSeguindo] = useState(true);
@@ -11,11 +18,7 @@ function App() {
     { nome: 'João Silva', tipo: 'PJ' },
     { nome: 'Maria Santos', tipo: 'CLT' },
     { nome: 'Pedro Almeida', tipo: 'PJ' },
-    // { nome: 'Ana Oliveira', tipo: 'CLT' },
-    // { nome: 'Lucas Costa', tipo: 'PJ' },
-    // { nome: 'Rafaela Martins', tipo: 'CLT' },
-    // { nome: 'Fernanda Souza', tipo: 'PJ' },
-  ])
+  ]);
 
   const handleAdicionar = () => {
     if (novoNome.trim()) {
@@ -23,14 +26,16 @@ function App() {
     }
 
     setNovoNome('');
-  }
+  };
 
   const handleRemover = (usuario) => {
-    const novosFuncionarios = funcionarios.filter((funcionario) => funcionario.nome !== usuario)
+    const novosFuncionarios = funcionarios.filter(
+      (funcionario) => funcionario.nome !== usuario,
+    );
 
-    setFuncionarios(novosFuncionarios)
-  }
-  
+    setFuncionarios(novosFuncionarios);
+  };
+
   const usuario = {
     nome: 'Gabriel Felix',
     ativo: true,
@@ -56,8 +61,8 @@ function App() {
       <div className="flex flex-col gap-4 border border-gray-700 py-2 px-50 rounded-2xl shadow-md shadow-blue-500/80">
         <h1 className="text-3xl font-bold mt-2">
           <div className="flex items-center gap-2 space-x-2">
-          <TvMinimal className='w-10 h-10'/>
-          Funcionários <span className="text-blue-500">Ternários.</span>
+            <TvMinimal className="w-10 h-10" />
+            Funcionários <span className="text-blue-500">Ternários.</span>
           </div>
         </h1>
         <p>
@@ -67,70 +72,96 @@ function App() {
         <div
           className={`font-medium text-lg ${usuario.online ? 'text-green-500' : 'text-red-500'}`}
         >
-          <div className='flex items-center gap-2'>            
-           <span> {usuario.nome}</span>
-           {usuario.ativo ? (
-            <ToggleRight className="w-6 h-6 text-green-500" />
-           ) : (
-            <ToggleLeft className="w-6 h-6 text-red-500" />
-           )}
+          <div className="flex items-center gap-2">
+            <span> {usuario.nome}</span>
+            {usuario.ativo ? (
+              <ToggleRight className="w-6 h-6 text-green-500" />
+            ) : (
+              <ToggleLeft className="w-6 h-6 text-red-500" />
+            )}
           </div>
         </div>
-        <div className='flex items-center gap-4'>
-        <input type="text" value={novoNome} onChange={(e) => setNovoNome(e.target.value)} placeholder="Digite o nome do funcionário" className="border border-gray-700 py-2 px-8 rounded-full w-80" />
-        <Plus onClick={handleAdicionar} className="w-10 h-10 cursor-pointer border border-gray-700 p-2 rounded-full" /> 
+        <div className="flex items-center gap-2 text-lg">
+          <span
+            className={`flex items-center gap-2 ${funcionarios.length > 0 ? 'text-green-500' : 'text-red-500'}`}
+          >
+            <User className="w-6 h-6" />
+            {funcionarios.length}
+          </span>
         </div>
+        <div className="flex items-center gap-4 mb-4">
+          <input
+            type="text"
+            value={novoNome}
+            onChange={(e) => setNovoNome(e.target.value)}
+            placeholder="Digite o nome do funcionário"
+            className="border border-gray-700 py-2 px-8 rounded-full w-80"
+          />
+          <Plus
+            onClick={handleAdicionar}
+            className="w-10 h-10 cursor-pointer border border-gray-700 p-2 rounded-full"
+          />
+        </div>
+        <h1 className="text-2xl text-center">
+          <span className="text-white-500">
+            {funcionarios <= 0 ? 'Não há funcionários cadastrados' : ''}
+          </span>
+        </h1>
         {isLoading ? (
           <p className="text-lg text-white">Carregando funcionarios...</p>
         ) : (
           <ul className="flex flex-col gap-2 mt-4 text-lg">
             {funcionarios.map((funcionario) => (
-              
               <li
                 key={funcionario.nome}
                 className="flex justify-between w-full border-b border-gray-700 py-2"
               >
-                <div className='flex gap-2'>
+                <div className="flex gap-2">
                   <User />
                   {`${funcionario.nome}`}
                 </div>
-                <div className='flex items-center gap-4'>
-                <span
-                  className={
-                    funcionario.tipo === 'CLT'
-                      ? 'text-blue-500'
-                      : 'text-yellow-500'
-                  }
-                >
-                  {funcionario.tipo}
-                </span>
-                <Trash2 className='w-6 h-6 text-red-500 hover:text-red-700 cursor-pointer' onClick={() => handleRemover(funcionario.nome)}/>
+                <div className="flex items-center gap-4">
+                  <span
+                    className={
+                      funcionario.tipo === 'CLT'
+                        ? 'text-blue-500'
+                        : 'text-yellow-500'
+                    }
+                  >
+                    {funcionario.tipo}
+                  </span>
+                  <Trash2
+                    className="w-6 h-6 text-red-500 hover:text-red-700 cursor-pointer"
+                    onClick={() => handleRemover(funcionario.nome)}
+                  />
                 </div>
               </li>
             ))}
-            <button
-              type="button"
-              onClick={() => setSeguindo(!seguindo)}
-              className="mx-auto block w-50 mt-6 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full cursor-pointer"
-            >
-              {seguindo ? 'Deixar de seguir' : 'Seguir'}
-            </button>
+            <div className="flex items-center justify-center gap-4">
+              <button
+                type="button"
+                onClick={() => setSeguindo(!seguindo)}
+                className="mx-auto block w-50 m-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full cursor-pointer"
+              >
+                {seguindo ? 'Deixar de seguir' : 'Seguir'}
+              </button>
 
-            <button
-              type="button"
-              className={`
-              mx-auto block w-50 mt-2 border border-blue-500 text-white font-bold py-2 px-4 rounded-full
+              <button
+                type="button"
+                className={`
+              mx-auto block w-50 m-4 border border-blue-500 text-white font-bold py-2 px-4 rounded-full
               ${
                 admin.permissao === 'admin'
                   ? 'hover:bg-blue-400/30 cursor-pointer'
                   : 'cursor-not-allowed opacity-50'
               }
             `}
-            >
-              {admin.permissao === 'admin'
-                ? 'Acessar Painel'
-                : 'Acesso Restrito'}
-            </button>
+              >
+                {admin.permissao === 'admin'
+                  ? 'Acessar Painel'
+                  : 'Acesso Restrito'}
+              </button>
+            </div>
           </ul>
         )}
       </div>
